1c30d6620e997b721a0dfe562c19356e63e92b5e
Rigo Armando Rosero Castillo <rigo@192.168.68.106>
2024-02-16 12:41:37 -0500

    initial config for the API

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import (
-	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/mvc"
-)
+import "github.com/kataras/iris/v12"
+
+func main() {
+	app := irisNewApp()
+
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+}
+
+func irisNewApp() *iris.Application {
+	app := iris.New()
+
+	app.Get("/", func(ctx iris.Context) {
+		ctx.Text("Hello MIgue kun")
+	})
+
+	return app
+}
